32a16a1df3b90bcebcea7b791d781c71331f0411
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-10-21 10:57:23 -0700

    move getCharacter to api directory

---
+++ src/api/get-character.ts
@@ -0,0 +1,23 @@
+// right now this is run on the client
+// i can just make this run on the server instead
+// TODO: implement server
+import { Character } from "@/types/index";
+
+export async function getCharacter(): Promise<Character | void> {
+  const URL = "https://rickandmortyapi.com/api/character/2";
+  try {
+    const response: Response = await fetch(URL);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: Character = await response.json();
+
+    // consider putting a type guard here
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("There was an error with the fetch operation:", error);
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,8 @@
-import { Character } from "@/types/index";
+import { getCharacter } from "@/api/get-character";
 
-async function getCharacter(): Promise<Character | void> {
-  const URL = "https://rickandmortyapi.com/api/character/2";
-  try {
-    const response: Response = await fetch(URL);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data: Character = await response.json();
-
-    // consider putting a type guard here
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error("There was an error with the fetch operation:", error);
-  }
-}
-
-// TODO: Access res properties
+// TODO:
+// Access res properties
+// - client side js goes here (DOM manipulation)
 function onDOMLoad() {
   document.addEventListener("DOMContentLoaded", async (): Promise<void> => {
     await getCharacter();
