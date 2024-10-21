6a23c884530ad8c093386626ea9270e526392a48
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-10-21 11:33:53 -0700

    install local typescript. rewrite scripts to use local ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { getCharacter } from "@/api/get-character";
 // TODO:
 // Access res properties
 // - client side js goes here (DOM manipulation)
+
 function onDOMLoad() {
   document.addEventListener("DOMContentLoaded", async (): Promise<void> => {
     await getCharacter();
