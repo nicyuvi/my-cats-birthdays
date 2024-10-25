788fa87c79495e662bfc493a3176cb533f94d2d1
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-10-24 20:05:44 -0700

    add comments

--- src/App_Start/RouteConfig.ts
+++ src/App_Start/RouteConfig.ts
@@ -1,6 +1,9 @@
+// import HomeController
 export class RouteConfig {
   public RegisterRoutes(): void {
-    console.log('register routes here edit')
+    console.log('register routes here')
     // list routes here
+    // each route will call a controller to handle logic
+    // req to /cats -> HomeController()
   }
 }
